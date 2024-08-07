ec0c5102668594252cad17170522adc4e27eb7ca
Aditya <adityajain_us@icloud.com>
2024-08-07 17:14:52 -0400

    Added two new functions to map.c, create map and a simple string hash for testing. Tommorow I will work on completing the map and testing, there might also be some extra functions added. This will relfect in the .h file

--- map.c
+++ map.c
@@ -1,13 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "map.h"
+
+#define STARTING_SIZE 4
+#define MAX_LOAD 1.7
+
+int simple_string_hash(void *string);
 
 
+void *create_map(int (*hash)(void*), int (*compare)(void*,void*)){
+    Map *main;
+    if(!(main = malloc(sizeof(Map)))){
+        return NULL;
+    }
+    main->compare = compare;
+    main->hash = hash;
+    main->capacity = STARTING_SIZE;
+    main->pairs = 0;
+    main->map = calloc(sizeof(Bucket*),STARTING_SIZE);
+    if (!main->map){
+        return NULL;
+    }
+    return main;
+}
 
 
 
 
 
 int main(){
-    printf("Hello world\n");
+    Map *test = create_map(NULL,NULL);
+    printf("%d",test->capacity);
+    char* simple_string = "bye bye world";
+    printf("This is the result of a simple hash: %d", simple_string_hash(simple_string));
+
     return 0;
+}
+
+int simple_string_hash(void *string){
+    int total = 0;
+    while(*(char *)(string) != '\0'){
+        total += *(char *)string;
+        string++;
+    }
+    return total;
 }
--- map.h
+++ map.h
@@ -2,15 +2,28 @@
 
 #define MAP_H
 
+typedef struct bucket{
+    void *key;
+    void *value;
+    struct bucket *next;
+} Bucket;
+
+typedef struct map{
+    void *map;
+    int (*hash)(void*);
+    int (*compare)(void*,void*);
+    int pairs, capacity;
+} Map;
+
 void *create_map(int (*hash)(void*), int (*compare)(void*,void*));
 
-int add_pair(void *key, void *value);
+int add_pair(void *key, void *value, void *map);
 
-int remove_pair(void *key);
+int remove_pair(void *key, void *map);
 
-void *get_value(void *key);
+void *get_value(void *key, void *map);
 
-int delete_map();
+int delete_map(void *map);
 
 
 #endif
